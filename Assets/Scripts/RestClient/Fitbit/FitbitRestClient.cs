92234716b79c03a535e0511d8d9cfb3252aff3a4
Yuhsiang Lu <luki0639@gmail.com>
2016-02-25 13:39:55 -0500

    Add test code for fitbit api

--- Assets/Scripts/RestClient/Fitbit/FitbitRestClient.cs
+++ Assets/Scripts/RestClient/Fitbit/FitbitRestClient.cs
@@ -28,28 +28,48 @@ public class PersistentLogin
 public class FitbitRestClient : MonoBehaviour {
 
 	/*
-	*Application.OpenURL
-	*https://github.com/gree/unity-webview
-	*https://community.fitbit.com/t5/Web-API/Connecting-FitBit-through-Unity-using-current-Plug-Ins-WebView/td-p/1051844
-	* https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=227FWT&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight
+	http://www.fiea.ucf.edu/#scope=nutrition+weight+location+social+heartrate+settings+sleep+activity+profile&user_id=4885CL&token_type=Bearer&expires_in=604800&access_token=eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0NTcwMjg4MzYsInNjb3BlcyI6Indwcm8gd2xvYyB3bnV0IHdzbGUgd3NldCB3aHIgd3dlaSB3YWN0IHdzb2MiLCJzdWIiOiI0ODg1Q0wiLCJhdWQiOiIyMjdGV1QiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJpYXQiOjE0NTY0MjQwMzZ9.JJYII-KGWk5Labva3PSCOnOcR-Xf4LwJPyg9ouHSH50
 	*/
 
 	private static string CLIENT_ID = "227FWT";
-	private static string SECRET = "474b1787965a0cc8e648660f40f3f1ce";
 	private static string LOG_URL = "https://www.fitbit.com/oauth2/authorize";
 	private static string TOKEN_URL = "https://api.fitbit.com/oauth2/token";
 
+	private static string USER_ID = "4885CL";
+	private static string ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0NTcwMjg4MzYsInNjb3BlcyI6Indwcm8gd2xvYyB3bnV0IHdzbGUgd3NldCB3aHIgd3dlaSB3YWN0IHdzb2MiLCJzdWIiOiI0ODg1Q0wiLCJhdWQiOiIyMjdGV1QiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJpYXQiOjE0NTY0MjQwMzZ9.JJYII-KGWk5Labva3PSCOnOcR-Xf4LwJPyg9ouHSH50";
+
 	private static string testurl = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id="+ CLIENT_ID+ "&redirect_uri=fitachi%3A%2F%2Fcb&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
+	private static string testurl2 = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=" + CLIENT_ID + "&redirect_uri=http%3A%2F%2Fwww.fiea.ucf.edu&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
 
 	// Use this for initialization
-	void Start () {
+	IEnumerator Start () {
 		//HTTPRestHelper<FitbitRestClient> request = new HTTPRestHelper<FitbitRestClient>("http://www.fiea.ucf.edu/", RestMethod.GET);
 		//request.ExecAysnc(test);
 
-		
+
 		//WWW www = new WWW("http://www.fiea.ucf.edu/");
 		//StartCoroutine(WaitForRequest(www));
-		Application.OpenURL(testurl);
+		string url = testurl;
+        try
+		{
+			AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+		}
+		catch
+		{
+			url = testurl2;
+        }
+
+		//Application.OpenURL(url);
+
+		// SEE here
+		var headers = new Dictionary<string, string>();
+		headers.Add("Authorization", "Bearer " + ACCESS_TOKEN);
+        var www = new WWW("https://api.fitbit.com/1/user/"+ USER_ID + "/profile.json", null, headers);
+
+		yield return www;
+
+		Debug.Log(www.text);
     }
 
 	void test<FitbitRestClient>(uint httpCode, FitbitRestClient data)
@@ -74,15 +94,20 @@ public class FitbitRestClient : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		try {
+			AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
 
-		AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-		AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-
-        if (activity.Call<bool>("IsLogin"))
+			if (activity.Call<bool>("IsLogin"))
+			{
+				Debug.Log(activity.Call<string>("GetToken"));
+				Debug.Log(activity.Call<string>("GetUserId"));
+				Debug.Log(activity.Call<string>("GetExpires"));
+			}
+		}
+		catch
 		{
-			Debug.Log(activity.Call<string>("GetToken"));
-			Debug.Log(activity.Call<string>("GetUserId"));
-			Debug.Log(activity.Call<string>("GetExpires"));
+
 		}
 	}
 
