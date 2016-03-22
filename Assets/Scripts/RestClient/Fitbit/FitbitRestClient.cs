573c0a14df49244bc0092fc692a2b46385be5b93
Yuhsiang Lu <luki0639@gmail.com>
2016-03-22 19:24:46 -0400

    Add interface for fitbit

--- Assets/Scripts/RestClient/Fitbit/FitbitRestClient.cs
+++ Assets/Scripts/RestClient/Fitbit/FitbitRestClient.cs
@@ -2,117 +2,120 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class PersistentLogin
-{
-	private const string TokenKey = "Token";
-	private const string UserIDKey = "UserID";
-
-	public static void SaveData(string token, int userID)
-	{
-		PlayerPrefs.SetString(TokenKey, token);
-		PlayerPrefs.SetInt(UserIDKey, userID);
-	}
-
-	public static Dictionary<string, object> LoadData()
-	{
-		var data = new Dictionary<string, object>();
-		string token = PlayerPrefs.GetString(TokenKey);
-		int userId = PlayerPrefs.GetInt(UserIDKey);
-		data.Add(TokenKey, token);
-		data.Add(UserIDKey, userId);
-		return data;
-	}
-}
-
-
 public class FitbitRestClient : MonoBehaviour {
 
-	/*
-	http://www.fiea.ucf.edu/#scope=nutrition+weight+location+social+heartrate+settings+sleep+activity+profile&user_id=4885CL&token_type=Bearer&expires_in=604800&access_token=eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0NTcwMjg4MzYsInNjb3BlcyI6Indwcm8gd2xvYyB3bnV0IHdzbGUgd3NldCB3aHIgd3dlaSB3YWN0IHdzb2MiLCJzdWIiOiI0ODg1Q0wiLCJhdWQiOiIyMjdGV1QiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJpYXQiOjE0NTY0MjQwMzZ9.JJYII-KGWk5Labva3PSCOnOcR-Xf4LwJPyg9ouHSH50
-	*/
+	private FitbitRestClient mInstance = null;
 
 	private static string CLIENT_ID = "227FWT";
-	private static string LOG_URL = "https://www.fitbit.com/oauth2/authorize";
-	private static string TOKEN_URL = "https://api.fitbit.com/oauth2/token";
+	private static string EXPIRED_TIME = "2592000";
+	private static string LOGIN_API = "https://www.fitbit.com/oauth2/authorize";
 
-	private static string USER_ID = "4885CL";
-	private static string ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0NTcwMjg4MzYsInNjb3BlcyI6Indwcm8gd2xvYyB3bnV0IHdzbGUgd3NldCB3aHIgd3dlaSB3YWN0IHdzb2MiLCJzdWIiOiI0ODg1Q0wiLCJhdWQiOiIyMjdGV1QiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJpYXQiOjE0NTY0MjQwMzZ9.JJYII-KGWk5Labva3PSCOnOcR-Xf4LwJPyg9ouHSH50";
+	private static string TokenKey = "Token";
+	private static string UserIDKey = "UserID";
 
-	private static string testurl = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id="+ CLIENT_ID+ "&redirect_uri=fitachi%3A%2F%2Fcb&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
-	private static string testurl2 = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=" + CLIENT_ID + "&redirect_uri=http%3A%2F%2Fwww.fiea.ucf.edu&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
+	private static string ANDROID_URL = LOGIN_API + "?response_type=token&client_id="+ CLIENT_ID+ "&redirect_uri=fitachi%3A%2F%2Fcb&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=" + EXPIRED_TIME;
+	private static string WIN_URL = LOGIN_API + "?response_type=token&client_id=" + CLIENT_ID + "&redirect_uri=http%3A%2F%2Fwww.fiea.ucf.edu&scope=activity%20nutrition%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=" + EXPIRED_TIME;
 
-	// Use this for initialization
-	IEnumerator Start () {
-		//HTTPRestHelper<FitbitRestClient> request = new HTTPRestHelper<FitbitRestClient>("http://www.fiea.ucf.edu/", RestMethod.GET);
-		//request.ExecAysnc(test);
+	private string mAccessToke = "";
+	private string mUserId = "";
 
+	private bool mIsLogin = false;
 
-		//WWW www = new WWW("http://www.fiea.ucf.edu/");
-		//StartCoroutine(WaitForRequest(www));
-		string url = testurl;
-        try
-		{
-			AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-			AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-		}
-		catch
-		{
-			url = testurl2;
-        }
+	public Fitbit.User User;
 
-		//Application.OpenURL(url);
-
-		// SEE here
-		var headers = new Dictionary<string, string>();
-		headers.Add("Authorization", "Bearer " + ACCESS_TOKEN);
-        var www = new WWW("https://api.fitbit.com/1/user/"+ USER_ID + "/profile.json", null, headers);
-
-		yield return www;
-
-		Debug.Log(www.text);
-    }
-
-	void test<FitbitRestClient>(uint httpCode, FitbitRestClient data)
+	public static void SaveData(string token, string userID)
 	{
-		Debug.Log(data);
+		PlayerPrefs.SetString(TokenKey, token);
+		PlayerPrefs.SetString(UserIDKey, userID);
 	}
 
-	IEnumerator WaitForRequest(WWW www)
+	public void LoadData()
 	{
-		yield return www;
-		// check for errors
-		if (www.error == null)
+		string mAccessToke = PlayerPrefs.GetString(TokenKey);
+		string mUserId = PlayerPrefs.GetString(UserIDKey);
+
+		if (mAccessToke == "" || mUserId == "")
 		{
-			Debug.Log("WWW Ok!: " + www.text);
+			mIsLogin = false;
 		}
 		else
 		{
-			Debug.Log("WWW Error: " + www.error);
+			mIsLogin = true;
 		}
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		mInstance = this;
+
+		// try get access toke from preference
+		LoadData();
+
+		if (!mIsLogin) {
+
+			string url = ANDROID_URL;
+			try
+			{
+				AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+				AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+			}
+			catch
+			{
+				url = WIN_URL;
+			}
+
+			Application.OpenURL(url);
+		}
+    }
+
+	Coroutine GetUser()
+	{
+		return StartCoroutine(GetUserInternal());
+	}
+
+	IEnumerator GetUserInternal()
+	{
+		var headers = new Dictionary<string, string>();
+		headers.Add("Authorization", "Bearer " + mAccessToke);
+		var www = new WWW("https://api.fitbit.com/1/user/" + mUserId + "/profile.json", null, headers);
+
+		while (!www.isDone)
+		{
+			yield return null;
+		}
+		Debug.Log(www.text);
+		User = JsonUtility.FromJson<Fitbit.User>(www.text);
+		Debug.Log("name " + User.fullName);
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		try {
-			AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-			AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-
-			if (activity.Call<bool>("IsLogin"))
-			{
-				Debug.Log(activity.Call<string>("GetToken"));
-				Debug.Log(activity.Call<string>("GetUserId"));
-				Debug.Log(activity.Call<string>("GetExpires"));
-			}
-		}
-		catch
+		if (!mIsLogin)
 		{
+			try
+			{
+				AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+				AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+				Debug.Log("Done");
+                if (activity.Call<bool>("IsLogin"))
+				{
+					mAccessToke = activity.Call<string>("GetToken");
+					mUserId = activity.Call<string>("GetUserId");
+					// TODO Debug.Log(activity.Call<string>("GetExpires"));
 
+					SaveData(mAccessToke, mUserId);
+					mIsLogin = true;
+				}
+			}
+			catch
+			{
+
+			}
+		} else
+		{
+			//GetUser();
 		}
-	}
-
-	public void Login()
-	{
-
 	}
 }
---
+++ Assets/Scripts/RestClient/Fitbit/User.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fitbit
+{
+
+	[System.Serializable]
+	public class User
+	{
+		public string aboutMe;
+		public string avatar;
+		public string avatar150;
+		public string city;
+		public string country;
+		public string dateOfBirthpublic;
+        public string displayName;
+		public string distanceUnit;
+		public string encodedId;
+		public string foodsLocale;
+		public string fullName;
+		public string genderpublic;
+        public string glucoseUnit;
+		public string height;
+		public string heightUnit;
+		public string locale;
+		public string memberSince;
+		public string nickname;
+		public string offsetFromUTCMillis;
+		public string startDayOfWeek;
+		public string state;
+		public string strideLengthRunning;
+		public string strideLengthWalking;
+		public string timezone;
+		public string waterUnit;
+		public string weight;
+		public string weightUnitpublic;
+	}
+}
