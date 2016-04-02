fa69258c3e29b46bbf66c767d49e1aa9d9f58bf5
Samuel Knox <sam@evorlor.com>
2016-04-02 19:37:04 -0400

    servers hate me

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -10,39 +10,4 @@ public class PlayerManager : ManagerBehaviour<PlayerManager>
     public const int StartingAttackPower = 10;
     public Player Player { get; set; }
     public int enemyHitPoints = 100;
-
-    private const float pollTime = 2.0f;
-
-    void Start()
-    {
-        //StartCoroutine(UpdatePlayer());
-    }
-
-    private IEnumerator UpdatePlayer()
-    {
-        while (true)
-        {
-            //ServerManager.Instance.GetPlayerJson(PlayerUpdated);
-            yield return new WaitForSeconds(pollTime);
-        }
-    }
-
-    private void PlayerUpdated(string results)
-    {
-        Debug.Log(results);
-        Player = JsonUtility.FromJson<Player>(results);
-        //var hitPointMonitor = FindObjectOfType<HitPointMonitor>();
-        //hitPointMonitor.UpdateHitpointText(Instance.Player.token, Instance.Player.hitPoints, enemyHitPoints);
-    }
-
-    //public int UserID { get; set; }
-    //public Nutrition Nutrition { get; set; }
-    //public Speed Speed { get; set; }
-    //public Endurance Endurance { get; set; }
-    //public Rest Rest { get; set; }
-
-    //protected override void Awake()
-    //{
-    //    UserID = 0;
-    //}
 }
