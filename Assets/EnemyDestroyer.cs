672fd3c90a7d0949779d5cd49be41185a39d1e39
Yuhsiang Lu <luki0639@gmail.com>
2016-04-07 13:35:28 -0400

    Adjust the ui

---
+++ Assets/EnemyDestroyer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class EnemyDestroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D attackedMonster)
+	{
+		if (attackedMonster.tag == "Monster")
+		{
+			Destroy(attackedMonster.gameObject);
+			SceneManager.LoadScene("Main");
+        }
+
+	}
+}
