678080f3d6f409ec322996e8864213ef540fb419
Samuel Knox <sam@evorlor.com>
2016-04-25 23:17:27 -0400

    date time setter

--- Assets/Scripts/Modules/AdventureStats.cs
+++ Assets/Scripts/Modules/AdventureStats.cs
@@ -2,7 +2,8 @@
 using System;
 using UnityEngine;
 
-public static class AdventureStats {
+public static class AdventureStats
+{
     public static int Gold;
 
     public static DateTime resetTime;
@@ -11,13 +12,13 @@ public static class AdventureStats {
 
     private static int[] AdventurerNutrition = new int[5];
 
-    private static int[] RecomendedNutrition = new int[5] { 3, 6, 3, 4, 7};
+    private static int[] RecomendedNutrition = new int[5] { 3, 6, 3, 4, 7 };
 
-	public static int Dairy;
+    public static int Dairy;
 
-	public static int Protein;
+    public static int Protein;
 
-	public static int Grain;
+    public static int Grain;
 
     public static int Vegetable;
 
@@ -31,78 +32,152 @@ public static class AdventureStats {
 
     public static int calories;
 
-    public static void Feed(FoodType food, int foodCaloires, int foodValue) {
+    public static Sex gender;
+    public static int DailyRecomendedCalories;
+
+    public static void SetRecomendedCalories(int age, Sex sex)
+    {
+        if (sex == Sex.Female)
+        {
+            if (age == 12)
+            {
+                DailyRecomendedCalories = 1800;
+            }
+            else if (age > 12 && age < 15)
+            {
+                DailyRecomendedCalories = 2000;
+            }
+            else if (age == 15)
+            {
+                DailyRecomendedCalories = 2200;
+            }
+            else if (age > 16 && age < 19)
+            {
+                DailyRecomendedCalories = 2400;
+            }
+            else if (age > 18 && age < 21)
+            {
+                DailyRecomendedCalories = 2600;
+            }
+            else
+            {
+                DailyRecomendedCalories = 2400;
+            }
+        }
+        if (sex == Sex.Male)
+        {
+            if (age < 14)
+            {
+                DailyRecomendedCalories = 1600;
+            }
+            else if (age > 13 && age < 19)
+            {
+                DailyRecomendedCalories = 1800;
+            }
+
+            else
+            {
+                DailyRecomendedCalories = 2000;
+            }
+
+        }
+    }
+
+    public static void Feed(FoodType food, int foodCaloires, int foodValue)
+    {
         AdventurerNutrition[(int)food] = foodValue;
         calories += foodCaloires;
     }
 
 
-    public static void UpdateSteps(int newStepsCount) {
-        if (fourDaysOfSteps.Count==4) {
+    public static void UpdateSteps(int newStepsCount)
+    {
+        if (fourDaysOfSteps.Count == 4)
+        {
             fourDaysOfSteps.Dequeue();
         }
         fourDaysOfSteps.Enqueue(newStepsCount);
         currentStepCount = newStepsCount;
         int averageHolder = 0;
-        foreach (int steps in fourDaysOfSteps) {
+        foreach (int steps in fourDaysOfSteps)
+        {
             averageHolder += steps;
         }
         averageStepsOverFourDays = averageHolder / 4;
 
     }
 
-    public static void Reset() {
-        for (int i = 0; i < AdventurerNutrition.Length; i++) {
+    public static void Reset()
+    {
+        for (int i = 0; i < AdventurerNutrition.Length; i++)
+        {
             AdventurerNutrition[i] = 0;
         }
         calories = 0;
     }
 
-    public static int GetStat(FoodType type) {
+    public static int GetStat(FoodType type)
+    {
         return AdventurerNutrition[(int)type];
     }
 
-    public static float GetCombatStrength() {
+    public static float GetCombatStrength()
+    {
         float combatStrength = 0;
         float totalOverage = 0;
-        for (int i = 0; i < AdventurerNutrition.Length; i++) {
+        for (int i = 0; i < AdventurerNutrition.Length; i++)
+        {
             float value = AdventurerNutrition[i] / RecomendedNutrition[i];
             combatStrength += (value < 0) ? 0 : (value > 1) ? 1 : value;
-            if (AdventurerNutrition[i] - RecomendedNutrition[i] > 0) {
+            if (AdventurerNutrition[i] - RecomendedNutrition[i] > 0)
+            {
                 totalOverage++;
             }
         }
         totalOverage = totalOverage * .05f;
 
-        combatStrength *= (1-totalOverage);
+        combatStrength *= (1 - totalOverage);
 
         return combatStrength;
     }
-    public static void checkForNewDate(){
-        if (DateTime.Now>resetTime) {
+
+    //update
+    public static void checkForNewDate()
+    {
+        if (DateTime.Now > resetTime)
+        {
             Reset();
             resetTime.AddDays(1);
         }
     }
-    public static void SetNewDate(){
-        if (!PlayerPrefs.HasKey("ResetYear")) {
+
+    //call on launch
+    public static void SetNewDate()
+    {
+        if (!PlayerPrefs.HasKey("ResetYear"))
+        {
             resetTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 4, 0, 0);
             PlayerPrefs.SetInt("ResetYear", DateTime.Now.Year);
             PlayerPrefs.SetInt("ResetMonth", DateTime.Now.Month);
             PlayerPrefs.SetInt("ResetDay", DateTime.Now.Day);
         }
-        if (PlayerPrefs.HasKey("ResetYear")) {
+        if (PlayerPrefs.HasKey("ResetYear"))
+        {
             resetTime = new DateTime(PlayerPrefs.GetInt("ResetYear"), PlayerPrefs.GetInt("ResetMonth"), PlayerPrefs.GetInt("ResetDay"), 4, 0, 0);
         }
     }
-    public static void SaveResetTime() {
+
+    //call on close
+    public static void SaveResetTime()
+    {
         PlayerPrefs.SetInt("ResetYear", resetTime.Year);
         PlayerPrefs.SetInt("ResetMonth", resetTime.Month);
         PlayerPrefs.SetInt("ResetDay", resetTime.Day);
     }
 
 }
-public enum Sex {
+public enum Sex
+{
     Male,
     Female
 }
--- Assets/Scripts/Modules/SexSelector.cs
+++ Assets/Scripts/Modules/SexSelector.cs
@@ -2,18 +2,21 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class SexSelector : MonoBehaviour {
+public class SexSelector : MonoBehaviour
+{
     public Image profilePic;
     public Sprite malePic;
 
-	// Use this for initialization
-	IEnumerator Start () {
+    // Use this for initialization
+    IEnumerator Start()
+    {
         yield return FitbitRestClient.GetProfile();
-	}
+    }
 
     private bool sexed = false;
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 
         if (sexed)
         {
@@ -21,13 +24,19 @@ public class SexSelector : MonoBehaviour {
         }
 
         var gender = FitbitRestClient.Profile.user.gender;
-        if(gender != null)
+        if (gender != null)
         {
             sexed = true;
             if (gender == "MALE")
             {
+                AdventureStats.gender = Sex.Male;
                 profilePic.sprite = malePic;
             }
+            else
+            {
+                AdventureStats.gender = Sex.Female;
+            }
+            AdventureStats.SetRecomendedCalories(20, AdventureStats.gender);
         }
     }
 }
