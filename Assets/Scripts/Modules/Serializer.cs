0ae7d415537e998e4d7c9f4f8b3dcdf7c1812dbb
Samuel Knox <sam@evorlor.com>
2016-04-25 21:18:13 -0400

    player serialization

---
+++ Assets/Scripts/Modules/Serializer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Serializer
+{
+    public static string SerializePlayer()
+    {
+        var playerData = new PlayerData();
+        playerData.Dairy = AdventureStats.Dairy;
+        playerData.Protein = AdventureStats.Protein;
+        playerData.Grain = AdventureStats.Grain;
+        playerData.Vegetable = AdventureStats.Vegetable;
+        playerData.Fruit = AdventureStats.Fruit;
+        playerData.Sweets = AdventureStats.Sweets;
+        string playerJson = JsonUtility.ToJson(playerData);
+        return playerJson;
+    }
+}
