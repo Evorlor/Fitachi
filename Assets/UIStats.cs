c94015923cbd88877ddcf218d6ee37b098264104
Samuel Knox <sam@evorlor.com>
2016-04-28 13:59:50 -0400

    stats pg

--- Assets/UIStats.cs
+++ Assets/UIStats.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 public class UIStats : MonoBehaviour
 {
     public Text stepsTxt;
+    public Text caloriesTxt;
 
     // Use this for initialization
     void Start()
     {
         stepsTxt.text = "Steps: " + FitbitRestClient.Instance.ActivitiesDaily.summary.steps;
+        caloriesTxt.text = "Calories Burnt: " + FitbitRestClient.Instance.ActivitiesDaily.summary.activityCalories;
     }
 
     // Update is called once per frame
