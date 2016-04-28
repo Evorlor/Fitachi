138efba2dd7fa30ead63bc899be2b9531b3bd5c1
Yuhsiang Lu <luki0639@gmail.com>
2016-04-28 11:58:05 -0400

    Update states

---
+++ Assets/StatUpdate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StatUpdate : MonoBehaviour {
+
+	[SerializeField]
+	private Text tom;
+	[SerializeField]
+	private Text littletom;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (FitbitRestClient.Instance.Activities.lifetime == null)
+		{
+			return;
+		}
+		tom.text = "Steps: "+ FitbitRestClient.Instance.Activities.lifetime.total.steps;
+		littletom.text = "Calories: " + FitbitRestClient.Instance.Activities.lifetime.total.caloriesOut;
+	}
+}
